351181f5523a0612ac838c01ea1b5dded24ec6b4
viliam <viliam.klein@gmail.com>
2022-10-18 11:23:19 -0600

    added function for getting half column

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <numeric>
+#include <algorithm>
 
 #include "TSimage2xy.h"
 
 std::vector<float> char2floatConv(std::vector<int16_t> inData);
+void testIndex(std::vector<float> imgData);
+
+template<typename T> double getAverage(std::vector<T> const& v);
+template<typename TT> std::vector<TT> getHalfCol(std::vector<TT> const& dataVec, int offset, int numRows, int width);
 
 int main(int argc, char *argv[]){
 
@@ -34,6 +40,7 @@ int main(int argc, char *argv[]){
 
     simplexy_t test;
     std::vector<float> tI(testImage.begin(), testImage.end());
+    testIndex(tI);
     test = tsImage2xy(tI, naxes[0], naxes[1]);
 
     // std::cout << "done with test\n";
@@ -49,6 +56,59 @@ int main(int argc, char *argv[]){
 
 }
 
+void testIndex(std::vector<float> imgData){
+
+    int width = 1024;
+    int indNumRows = 512;
+    int colOffset = 0;
+
+    // std::vector<float> halfCol(512, 0);
+
+    // for(int ii = 0; ii < indNumRows; ii++){
+        
+    //     indArray = ii * 1024 + colOffset;
+    //     halfCol[ii] = imgData[indArray];
+    //     // std::cout << halfCol[ii] << "\n";
+    // }
+
+    std::vector<int> inddata(width, 0);
+    std::iota(inddata.begin(), inddata.end(), 0);
+    
+    std::vector<float> halfCol = getHalfCol(imgData, colOffset, indNumRows, width);
+
+    double avg = getAverage(halfCol);
+    std::cout << avg << "\n";
+}
+
+template<typename TT>
+std::vector<TT> getHalfCol(std::vector<TT> const& dataVec, int offset, int numRows, int width){
+
+    std::vector<TT> halfCol(numRows, 0);
+    // std::vector<TT>::iterator it;
+    // auto itData = dataVec.begin();
+    int indArray = 0;
+    int ii = 0;
+
+    for(auto itData = dataVec.begin(); itData != dataVec.end(); std::advance(itData, width), ii++){
+        halfCol[ii] = *itData;
+    }
+
+    // for(int ii = 0; ii < numRows; ii++){
+    //     indArray = ii * width + offset;
+    //     halfCol[ii] = dataVec[indArray];
+    // }
+
+    return halfCol;
+}
+
+template<typename T>
+double getAverage(std::vector<T> const& v) {
+    if (v.empty()) {
+        return 0;
+    }
+ 
+    return std::reduce(v.begin(), v.end(), 0.0) / v.size();
+}
 
 std::vector<float> char2floatConv(std::vector<int16_t> inData){
 
